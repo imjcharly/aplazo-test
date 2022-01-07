55487bc93844d3b38d6862aea6989330a605020e
iti_jccj <iti.jccj@gmail.com>
2022-01-07 01:35:54 -0600

    adds routes with lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rick-and-morty',
+    loadChildren: () => import('./modules/rickandmorty/rickandmorty.module').then(m => m.RickandmortyModule)
+  },
+  { path: '**', redirectTo: 'rick-and-morty' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
